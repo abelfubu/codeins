b3860327a24d44400f142b0b6e5335e62c0a81a0
abelfubu <abelfubu@gmail.com>
2020-06-08 22:45:39 +0200

    d

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import SideBar from './components/SideBar/SideBar';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import Form from './components/Form/Form';
 
 function App() {
   return (
     <div className='App'>
-      <HashRouter>
+      <BrowserRouter>
         <SideBar />
         <Form />
-      </HashRouter>
+      </BrowserRouter>
     </div>
   );
 }
